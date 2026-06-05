a6b3fc50e439b8bee8bd59491c9c4b66ffcc11f7
abhishek sindhwal <abhisheksindhwal11@gmail.com>
2026-06-05 21:03:14 +0530

    service later added in front-end

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { AuthContext } from "../auth.context";
+import { login,register,logout,getMe } from "../services/auth.api";
+
+
+
+export const useAuth=()=>{
+    const context = useContext(AuthContext)
+
+    const {user,setUser,loading,setLaoding}=context
+}
