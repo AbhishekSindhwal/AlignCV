a0a467c4ac14d59665884005f790b990489eef51
abhishek sindhwal <abhisheksindhwal11@gmail.com>
2026-06-06 22:58:43 +0530

    hooks functionality added

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,15 +1,14 @@
 import { useState, createContext } from "react";
 
-export const AuthContext=createContext()
+export const AuthContext = createContext()
 
-
-export const AuthProvider = ({children})=>{
-    const [user,serUser]=useState(null)
-    const [loading, setLoading]=useState(false)
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
 
 
     return (
-        <AuthContext.Provider value={{user,setUser,loading,setLoading}}>
+        <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
             {children}
         </AuthContext.Provider>
     )
